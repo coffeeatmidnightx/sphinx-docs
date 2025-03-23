443f9fc0cd78a978885f7b7ece37ef717a46eafc
Jana <noneofyourbusiness@yall.com>
2025-03-23 13:54:31 +0100

    Initial commit

---
+++ build/_sources/index.rst.txt
@@ -0,0 +1,4 @@
+Data Platform Technical Documentation
+========================================
+
+This is the technical documentation of a data platform, written in reStructuredText (reST) and Sphinx.
---
+++ source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Data Platform Documentation'
+copyright = '2025, Jana'
+author = 'Jana'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+html_title = "Data Platform Documentation"
+
+html_theme_options = {
+    "announcement": "Release 1.0.0 coming soon!",
+}
